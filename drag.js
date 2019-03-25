3228ee17b5f0f6c8ae1788ec1530799ab66ea56e
liwanyue <2450746759@qq.com>
2019-03-25 17:37:48 +0800

    拖拽上传

--- drag.js
+++ drag.js
@@ -2,9 +2,21 @@
  * Created by Administrator on 2019/3/25.
  */
 function drag(id){
-    var obj = document.getElmentById(id);
+    var obj = document.getElementById(id);
     var disX = 0;
     var disY = 0;
-    obj.onmousedown = function(){
-    }
+    obj.onmousedown = function(e){
+        var ev = e || event;
+        disX = ev.pageX - obj.offsetLeft;
+        disY = ev.pageY - obj.offsetTop;
+        document.onmousemove = function(e){
+            var ev = e || event;
+            obj.style.left = ev.pageX - disX + "px";
+            obj.style.top = ev.pageY - disY + "px";
+        };
+        document.onmouseup = function(){
+            document.onmousemove = document.onmouseup = null;
+        };
+    };
+    return false;
 }
